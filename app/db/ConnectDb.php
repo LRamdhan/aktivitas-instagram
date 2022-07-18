ba7b3bfeb8d44ca8c2246becc3d9aa1d181e2839
ubed <ijul@gmail.com>
2022-07-18 14:52:10 +0700

    perbaikan sintaks

--- app/db/ConnectDb.php
+++ app/db/ConnectDb.php
@@ -7,7 +7,8 @@ class ConnectDb {
     protected $connect;
 
     public function __construct() {
-        $this->connect = mysqli_connect('sql304.epizy.com', 'epiz_32190553', 'UCvdZ2Ztb5np', 'epiz_32190553_aktivitas_instagram');        if(!$this->connect) {
+        $this->connect = mysqli_connect('sql304.epizy.com', 'epiz_32190553', 'UCvdZ2Ztb5np', 'epiz_32190553_aktivitas_instagram');        
+        if(!$this->connect) {
             throw new DbError('terjadi kesalahan pada saat koneksi ke database');
         }
     }
